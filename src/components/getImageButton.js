2012068ea09ed6e96deb30850bccdd116d10ae9e
shaunhailey <ediblelysol@gmail.com>
2017-08-22 18:01:08 -0400

    not rendering

---
+++ src/components/getImageButton.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const getImageButton = ({ onClick }) => <button onClick={onClick}>Get Images</button>
+
+export default getImageButton
---
+++ src/components/getImageForm.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import getImageButton from './getImageButton'
+import ImageDisplay from './ImageDisplay'
+const API_KEY = 'UnMyhplRWTBlTHoaEBlnjms5yIzaUERnAxGQeCkW'
+
+class getImageForm extends Component {
+  state = {
+    rover: 'Curiosity',
+    camera: 'FHAZ',
+    sol: 1000,
+    photos: []
+  }
+
+  _change = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  _fetchRoverImage = event => {
+    event.preventDefault()
+    const { rover, camera, sol } = this.state
+    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&camera=${camera}&api_key=${API_KEY}`
+
+    fetch(url).then(r => r.json()).then(({ photos }) => {
+      this.setState({ photos })
+    })
+  }
+  render() {
+    return (
+      <div className="getImageForm">
+        <form>
+          <label htmlFor="rover">Rover</label>
+          <select onChange={this._change} name="rover" value={this.state.rover}>
+            <option value="Curiosity">Curiosity</option>
+            <option value="Opportunity">Opportunity</option>
+            <option value="Spirit">Spirt</option>
+          </select>
+          <label htmlFor="camera">Camera Type</label>
+          <select onChange={this._change} name="camera" value={this.state.camera}>
+            <option value="fhaz">FHAZ (Front Hazard)</option>
+            <option value="rhaz">RHAZ (Rear Hazard)</option>
+            <option value="navcam">NAVCAM (Navigation Cam)</option>
+          </select>
+          <label htmlFor="sol">Martian Sol: 1000-2000</label>
+          <input type="number" onChange={this._change} name="sol" max="2000" min="1000" value={this.state.sol} />
+        </form>
+        <getImageButton onClick={this._fetchRoverImage} />
+        <ImageDisplay images={this.state.photos} />
+      </div>
+    )
+  }
+}
+export default getImageForm
